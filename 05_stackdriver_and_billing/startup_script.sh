3e0ba4758618b108d107a00e8d223b5e9fbde650
Chad Smith <csmith@MacBook-Pro-2.local>
2020-02-27 12:30:48 -0700

    updated several demos

---
+++ 05_stackdriver_and_billing/startup_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
+sudo bash install-monitoring-agent.sh
+curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
+sudo bash install-logging-agent.sh
+
